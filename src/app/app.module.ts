27d161a03ce81284dbb95b41477036fd1b25e939
philoem <philoem@nexgate.ch>
2018-09-26 18:22:06 +0200

    mise en place structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { HeaderComponent } from './header/header.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { PostFormComponent } from './posts-list/post-form/post-form.component';
+import { PostService } from './services/post.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SinglePostComponent } from './posts-list/single-post/single-post.component';
+
+const appRoutes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'post', component: PostsListComponent },
+  { path: 'post/new', canActivate: [AuthGuardService],component: PostFormComponent },
+  { path: 'post/view/:id', canActivate: [AuthGuardService],component: SinglePostComponent },
+  { path: '', redirectTo: 'post', pathMatch: 'full' },
+  { path: '**', redirectTo: 'post' }
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    HeaderComponent,
+    PostsListComponent,
+    PostFormComponent,
+    SignupComponent,
+    SigninComponent,
+    SinglePostComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    PostService,
+    AuthGuardService
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/post.model.ts
@@ -0,0 +1,9 @@
+export class Post {
+
+    date: Date;    
+    
+    constructor(public title: string, public content: string) {
+        
+    }
+
+}
